e9b586d6a4249ca1d5c8bf1a808c0be66dd47c01
Pooja14206 <pooja.14206@gmail.com>
2021-10-15 02:06:33 +0530

    added employee payroll

---
+++ src/component/add/Add.jsx
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react'
+// import Header from '../header/Header'
+
+// export default class add extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Header></Header>
+//                 <h1>You r inside Add file</h1>
+//             </div>
+//         )
+//     }
+// }
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,19 @@
+
+// import React, { Component } from 'react';
+// import './Header.css';
+
+// export default class header extends Component {
+//   render() {
+//     return (
+//       <div className='header-content header'>
+//         <div className='logo-content'>
+//           <img src='./assets/logo.png' alt='Logo'/>
+//           <div className="logo-text-content">
+//             <span className='mp-text'>EMPLOYEE</span>
+//             <span className='emp-text emp-payroll'>PAYROLL</span>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
