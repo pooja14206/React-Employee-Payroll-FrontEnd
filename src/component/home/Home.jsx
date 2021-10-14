27f42ead1fd353500badccaaf127a1e5d0159892
Pooja14206 <pooja.14206@gmail.com>
2021-10-15 02:12:07 +0530

    added: Header, and routing

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,32 +1,32 @@
-// import React, { Component } from 'react';
-// import Header from '../header/Header';
-// import './Home.css';
-// import {useHistory} from 'react-router-dom';
+import React, { Component } from 'react';
+import Header from '../header/Header';
+import './Home.css';
+import {useHistory} from 'react-router-dom';
 
-// export default class home extends Component {
+export default class home extends Component {
 
-//     emoloyeeCount = 0;
+    emoloyeeCount = 0;
 
-//   render() {
-//     return (
-//       <div>
-//         <Header></Header>
-//         {/* <h1>You are inside Home</h1> */}
-//         <div className="main-content">
-//           <div className="header-content">
-//             <div className="emp-detail-text">
-//               Employee Details <div className="emp-count">{this.emoloyeeCount}</div>
-//             </div>
-//             {/* add button redirect to the add page to add a new employee */}
-//             {/* <Link className="add-button" href="/add" > */}
-//               {/* <img src="" alt="Add User Logo"> */}
-//                 Add User
-//             {/* </Link>     */}
-//               {/* </img> */}
-//             {/* </a> */}
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        {/* <h1>You are inside Home</h1> */}
+        <div className="main-content">
+          <div className="header-content">
+            <div className="emp-detail-text">
+              Employee Details <div className="emp-count">{this.emoloyeeCount}</div>
+            </div>
+            {/* add button redirect to the add page to add a new employee */}
+            {/* <Link className="add-button" href="/add" > */}
+              {/* <img src="" alt="Add User Logo"> */}
+                Add User
+            {/* </Link>     */}
+              {/* </img> */}
+            {/* </a> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/component/routing/Routing.jsx
+++ src/component/routing/Routing.jsx
@@ -1,15 +1,15 @@
-// import React, { Component } from "react";
-// import Home from "../home/Home";
-// import Add from "../add/Add";
-// import { BrowserRouter as Router, Route} from "react-router-dom";
+import React, { Component } from "react";
+import Home from "../home/Home";
+import Add from "../add/Add";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-// export default class Routing extends Component {
-//   render() {
-//     return (
-//       <Router>
-//         <Route exact path="/" component={Home} />
-//         <Route path="/add" component={Add} />
-//       </Router>
-//     );
-//   }
-// }
+export default class Routing extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/add" component={Add} />
+      </Router>
+    );
+  }
+}
